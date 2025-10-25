b0c562bec07728108d35e76f4feed01939d60a6c
Luis Canada <canadaluis5@gmail.com>
2025-10-25 03:58:23 -0400

    Add sonner tooltip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/query";
 import { ThemeProvider } from "./components/ThemeProvider";
+import { Toaster } from "#/ui/sonner";
 import Main from "./components/Main";
 
 export default function App() {
@@ -10,6 +11,8 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <Main />
+
+        <Toaster />
       </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
