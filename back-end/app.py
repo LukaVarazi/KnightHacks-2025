abeab0c40c62722e6f2088ce2856dc110db52a04
Luis Canada <canadaluis5@gmail.com>
2025-10-26 10:47:39 -0400

    docker change

--- back-end/app.py
+++ back-end/app.py
@@ -1025,4 +1025,4 @@ if __name__ == "__main__":
     app.logger.setLevel(logging.INFO)
 
     # Run Flask application
-    app.run(debug=True, port=5000, use_reloader=False)
+    app.run(debug=False, port=80, use_reloader=False)
